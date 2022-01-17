6af0ae827ed1f6e84e6f0a19eb7e331e1825bed3
Davide Tavella <tavella01@gmail.com>
2022-01-17 17:12:50 +0100

    database

---
+++ database.php
@@ -0,0 +1,42 @@
+<?php
+
+   $movies = [
+      [
+         "nome" => "Monster & Co.",
+         "cast" => ["Gassmann, Lollobrigida, Loren, Troisi, Proietti, Mastroianni"],
+         "regista" => "Walt Disney",
+         "riassunto" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis in eleifend lorem, a vehicula erat. Aenean volutpat eros leo, quis lobortis est tincidunt ac. Vivamus fermentum odio lectus, ut posuere diam bibendum et. Sed enim elit, faucibus nec dui quis, luctus faucibus magna. Nulla porta semper ipsum nec ullamcorper. Proin id nulla vitae ipsum ultricies accumsan. Praesent pretium dui non nisl rutrum placerat. Nullam rhoncus molestie augue eu sodales. ",
+         "anno_di_uscita" => 2019,
+      ],
+      [
+         "nome" => "Fast and Furious",
+         "cast" => ["Gassmann, Lollobrigida, Loren, Troisi, Proietti, Mastroianni"],
+         "regista" => "Gabriele Muccino",
+         "riassunto" => "Nulla facilisi. Sed congue nulla ipsum, vel pharetra odio scelerisque quis. Curabitur sed tempus est. Quisque eget pulvinar felis, sit amet cursus justo. Mauris aliquam dapibus pulvinar. Praesent eget aliquet tortor, non egestas justo. Integer eu tellus mattis erat tincidunt pretium vel a mi.",
+         "anno_di_uscita" => 2015,
+      ],
+      [
+         "nome" => "Natale in India",
+         "cast" => ["Gassmann, Lollobrigida, Loren, Troisi, Proietti, Mastroianni"],
+         "regista" => "Dario Argento",
+         "riassunto" => "Mauris nec nisl nec diam imperdiet ornare. Donec vitae imperdiet justo, viverra aliquam urna. Donec sed elit vel diam scelerisque dictum sed at tortor. Quisque sit amet fermentum elit. Aenean scelerisque, est at volutpat accumsan, tortor nulla venenatis dui, a semper erat enim vel arcu. Nullam a nisl odio. Phasellus sed est sed leo viverra vestibulum quis et sapien. Integer nec lacus vel arcu pretium pretium at at erat.",
+         "anno_di_uscita" => 2016,
+      ],
+      [
+         "nome" => "Rush",
+         "cast" => ["Gassmann, Lollobrigida, Loren, Troisi, Proietti, Mastroianni"],
+         "regista" => "Paolo Sorrentino",
+         "riassunto" => "Duis pharetra turpis vel lorem fermentum, at condimentum nisi posuere. Etiam accumsan nibh ut dui molestie tincidunt. In pharetra laoreet eros quis vehicula. Mauris tristique aliquet ultrices. Etiam vel blandit est. Fusce sagittis faucibus odio sit amet interdum. Sed ligula ante, egestas fermentum rhoncus et, hendrerit eget ipsum. Donec nec sollicitudin eros, eu placerat sapien. Morbi vulputate fermentum ornare. Ut ut ornare erat. Aenean finibus sem quis metus lacinia, id iaculis ante aliquam. Maecenas lobortis elementum vehicula. Quisque porta, velit ut efficitur vulputate, nisl diam rhoncus massa, id cursus massa ante mattis est. Nullam vehicula sem non sem consectetur dapibus.",
+         "anno_di_uscita" => 2012,
+      ],
+      [
+         "nome" => "La vita è bella",
+         "cast" => ["Gassmann, Lollobrigida, Loren, Troisi, Proietti, Mastroianni"],
+         "regista" => "Matteo Garrone",
+         "riassunto" => "Nunc tincidunt porttitor leo. Mauris pulvinar eleifend diam eget pellentesque. Ut in lacus ut odio fermentum tincidunt et id ligula. Etiam aliquam venenatis laoreet. Quisque accumsan augue id dolor fermentum, a dignissim orci aliquet. Nam vitae ex ac purus eleifend sodales. Donec nec mi et eros cursus aliquet. Mauris tortor elit, mollis id dignissim id, vestibulum nec nisl. Mauris sollicitudin sollicitudin massa sed tempor. ",
+         "anno_di_uscita" => 2010,
+      ],
+   ];
+
+
+   ?>
